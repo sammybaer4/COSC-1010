d43c08ec457f09fc23218f03603a6fcfe5dd2eb4
null <sambaer@westernwyoming.edu>
2026-02-03 19:56:54 +0000

    Completed Assignment

--- Sales-Prediction/main.py
+++ Sales-Prediction/main.py
@@ -1,14 +1,20 @@
 #
-# Name
-# Date
+# Sam Baer
+# 2/03/2026
 # Sales Prediction Programming Project
 # COSC 1010
 #
 
 # Variables to hold the sales total and the profit
+total_sales=0
+profit=0 
 
 # Get the amount of projected sales.
+total_sales = float(input("Enter the projected total sales: "))
 
-# Calculate the projected profit.
+#Calculate the projected profit.
+profit = total_sales * 0.23
 
-# Print the projected profit.
+# Print the projected profit
+print(f"The projected profit is: ${profit:,.2f}")
+       
