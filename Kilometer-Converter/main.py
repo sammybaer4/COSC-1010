c3b97879b7d89a62c479fa18fee90368b4d6cb88
null <sambaer@westernwyoming.edu>
2026-02-11 22:27:52 +0000

    Completed assingment

--- Kilometer-Converter/main.py
+++ Kilometer-Converter/main.py
@@ -1,7 +1,14 @@
 #
-# Name
-# Date
+# Sam 
+# 2/11/26
 # Kilometer Converter Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+# Kilometer to Miles Converter
+
+kilometers = float(input("Enter distance in kilometers: "))
+
+miles = kilometers * 0.6214
+
+print("Distance in miles:", miles)
