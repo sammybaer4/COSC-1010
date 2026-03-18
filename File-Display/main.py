5c9e2ac00d822b7ec77a1e185ec67ca303841142
student <student@local>
2026-03-18 01:23:20 +0000

    Completed asignment

--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,17 @@
 #
-# Name
-# Date
-# File Display Programming Project
+# sam
+# 3/17/26
+# File Display Programming Projecint
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+# Open the file 
+myfile = open('numbers.txt', 'r')
+
+# Read and disply  the file's contents.
+for line in myfile:
+    number =int(line)
+    print(number)
+
+# Close the file.
+myfile.close()
