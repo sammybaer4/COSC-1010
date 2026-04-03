04671651a8a5cb9bf613287f202cba03c1df4047
student <student@local>
2026-04-03 12:49:27 +0000

    completed assignment

--- Lottery-Number-Generator/main.py
+++ Lottery-Number-Generator/main.py
@@ -1,7 +1,31 @@
 #
-# Name
-# Date
+# sam
+# 4/3/26
 # Lottery Number Generator Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+import random
+
+MAX_DIGITS = 7 # Max number of digits
+START = 0 # Start of the random number range
+END = 9 # End of the random number range
+
+# main function
+def main():
+    # Create a list.
+    numbers = [0] * 7
+
+    # Populate the list with random numbers.
+    for index in range(MAX_DIGITS):
+         numbers[index] = random.randint(START, END)
+
+    # Display the random numbers.
+    print('Here are your lottery numbers:')
+    for index in range(MAX_DIGITS):
+         print(numbers[index], end='')
+    print()
+
+# Call the main function.
+main()
