aa79ffffd79b8c1abb7f30d43092dd8901e7398a
student <student@local>
2026-04-08 17:12:36 +0000

    completed assignment

--- Magic-8-Ball/main.py
+++ Magic-8-Ball/main.py
@@ -5,3 +5,26 @@
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+import random
+
+def main():
+    responses = []
+
+    # this part reads your text file into a list
+    infile = open('8_ball_responses.txt', 'r')
+    for line in infile:
+        responses.append(line.strip())
+    infile.close()
+
+    keep_going = 'y'
+    while keep_going.lower() == 'y':
+        input('Ask the Magic 8 Ball a yes/no question: ')
+
+    # this picks a random answer from your file
+        print(random.choice(responses))
+
+        keep_going = input('\nDo you have another question? (y/n): ')
+
+# this is the "start button" that tells python to run the main function
+if __name__ == '__main__':
+    main()
